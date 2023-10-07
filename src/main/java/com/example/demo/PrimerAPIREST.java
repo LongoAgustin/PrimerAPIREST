725d47c1a07c2444737592a6572a765b79adf69b
aguslongo69 <63927600+aguslongo69@users.noreply.github.com>
2023-10-07 20:56:15 -0300

    Version 2

--- src/main/java/com/example/demo/PrimerAPIREST.java
+++ src/main/java/com/example/demo/PrimerAPIREST.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrimerApiRestApplication {
+public class PrimerAPIREST {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(PrimerApiRestApplication.class, args);
+		SpringApplication.run(PrimerAPIREST.class, args);
 		System.out.println("Programa Corriendo");
 	}
 
